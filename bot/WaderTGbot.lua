787bef3584d244e8e91553f94abaa6c9c3476eeb
alirezaMB <instagramfree3@gmail.com>
2016-04-28 21:29:18 +0430

    Update WaderTGbot.lua

--- bot/WaderTGbot.lua
+++ bot/WaderTGbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {67559249,71951412,154868817,143531868,0,tonumber(our_id)},--Sudo users
+    sudo_users = {(204685466)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[WaderTG v4
